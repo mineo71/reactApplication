99660c61342f0e768f7b0ecfbf023d68f5f76a40
Oleh Rylskyj <olehrylskyj@gmail.com>
2022-12-23 03:52:07 +0200

    uploading pr 3
    react pr 3

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '',
+        'X-RapidAPI-Host': ''
+    }
+};
+
+export const GEO_API_URL = "";
+
+export const WEATHER_API_URL = "";
+export const WEARTHER_API_KEY = "";
